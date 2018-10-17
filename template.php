6ec79da6ebaa4c12aecddf1834358b90a233504b
Kelsey Amyotte <Kelsey Lorenz>
2018-10-16 19:06:34 -0500

    added generic header/footer files and changed template

--- template.php
+++ template.php
@@ -1,13 +1,4 @@
-<?php include("includes/a_config.php");?>
-<!DOCTYPE html>
-<html>
-<head>
-	<?php include("includes/head-tag-contents.php");?>
-</head>
-<body>
-
-<?php include("includes/design-top.php");?>
-<?php include("includes/navigation.php");?>
+<?php include("includes/header.php");?>
 
 <div class="container" id="main-content">
 	<h2>Welcome to my website!</h2>
@@ -22,6 +13,3 @@
 </div>
 
 <?php include("includes/footer.php");?>
-
-</body>
-</html>
