64fcfa8d579a186ec0003a981c205798cb5f128f
Kelsey Amyotte <Kelsey Lorenz>
2018-11-08 14:14:18 -0600

    Separated header and footer into different files so we can reuse them. Also created files for the other links in the header.

--- shared/footer.php
+++ shared/footer.php
@@ -1,2 +1,63 @@
+<!-- Footer ____________________________ -->
+<footer>
+    <div class="container">
+        <div class="top-footer row">
+            <div class="col-md-3 col-xs-12 footer-logo"> <a href="#"><img src="images/them-logo/them-main-logo-1.png" alt="Logo"></a> </div>
+            <!-- /.footer-logo -->
+
+            <div class="col-md-9 col-xs-12 footer-text">
+                <p>SAVING PAWS ANIMAL RESCUE INC. <br>
+                    Mail: PO Box 0362, Appleton, WI 54912-0362 <span class="spacing">|</span> Rescue: N3141 Meade Street, Appleton, WI 54913 <br>
+                    Phone: (920) 830-2392 <span class="spacing">|</span> Email: info@savingpaws.com <span class="spacing">|</span> We are a 501(c)3 non-profit organization
+                    All donations are tax deductible  74-1381999<span class="spacing">|</span>DS # 268579 </p>
+            </div>
+
+            <div class="col-md-12 col-xs-12 bottom-footer">
+
+                <h5>Join Our Mailing List</h5>
+                <form action="#">
+                    <input type="text" placeholder="Your Email">
+                    <button class="tran3s p-bg-color">Subscribe</button>
+                </form>
+            </div>
+        </div>
+        <!-- /.top-footer -->
+    </div>
+    <!-- /.container -->
+
+    <div class="bottom-footer">
+        <div class="container">
+            <p>Copyright &copy; 2018 <strong>SavingPaws</strong> SAVING PAWS Animal Rescue, Inc. All Rights Reserved</p>
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.bottom-footer -->
+</footer>
+</div>
+<!-- /.main-page-wrapper -->
+
+<!-- Scroll Top Button -->
+<button class="scroll-top tran7s p-color-bg"> <i class="fa fa-angle-up" aria-hidden="true"></i> </button>
+
+<!-- pre loader  -->
+<div id="loader-wrapper">
+    <div id="loader"></div>
+</div>
+<!-- js file -->
+<!-- Main js file/jquery -->
+<script src="vendor/jquery-2.2.3.min.js"></script>
+<!-- bootstrap-select.min.js -->
+<script src="vendor/bootstrap-select-1.10.0/dist/js/bootstrap-select.min.js"></script>
+<!-- bootstrap js -->
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- Slider js -->
+<script src="vendor/Camera-master/scripts/camera.min.js"></script>
+<!-- Owl carousel -->
+<script src="vendor/OwlCarousel2/dist/owl.carousel.min.js"></script>
+<!-- WOW js -->
+<script type="text/javascript" src="vendor/WOW-master/dist/wow.min.js"></script>
+<!-- Style js -->
+<script src="js/custom.js"></script>
 </body>
+
 </html>
