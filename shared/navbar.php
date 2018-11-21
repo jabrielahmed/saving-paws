24206278f4156944b1abe6f682723d57af7d3eb6
Kelsey Amyotte <Kelsey Lorenz>
2018-11-21 15:40:21 -0600

    Added calendar. Also separated navbar and header so we can insert custom CSS in each page

---
+++ shared/navbar.php
@@ -0,0 +1,58 @@
+</head>
+<?php session_start(); ?>
+<body>
+<div class="main-page-wrapper">
+
+    <!-- Header _________________________________ -->
+    <section class="header-section">
+        <div class="top-header">
+            <div class="container">
+                <div class="clear-fix">
+                    <ul class="float-left top-header-left">
+                        <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i> PO Box 0362, Appleton, WI 54912-0362</a></li>
+                        <li><a href="#"><i class="fa fa-envelope-o" aria-hidden="true"></i> info@savingpaws.com</a></li>
+                    </ul>
+                    <ul class="float-right top-header-right">
+                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <!-- Theme Main Menu ____________________________ -->
+        <div class="theme-main-menu">
+            <div class="container">
+                <div class="main-menu clear-fix">
+                    <div class="them-logo float-left"><a href="#"><img src="images/them-logo/them-main-logo-1.png" alt="logo"></a></div>
+
+                    <!-- Menu -->
+                    <nav class="navbar">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed tran3s" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false"> <span class="sr-only tran3s">Toggle navigation</span> <span class="icon-bar tran3s"></span> <span class="icon-bar tran3s"></span> <span class="icon-bar tran3s"></span> </button>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                <li class="dropdown-holder active current-page-item Active-manu"><a href="index.php"> Home </a> </li>
+                                <li class="dropdown-holder"><a href="events.php">Events &amp; More</a> </li>
+                                <li class="dropdown-holder"><a href="adoptable-pets.php">Adoptions</a> </li>
+                                <li class="dropdown-holder"><a href="sparked.php">S.P.A.R.K.E.D</a> </li>
+                                <li class="dropdown-holder"><a href="help.php">Help Saving Paws</a> </li>
+                                <li class="dropdown-holder"><a href="about.php">About Us</a> </li>
+                                <li><a href="contact-us.html">Thank You</a></li>
+                            </ul>
+                        </div>
+                        <!-- /.navbar-collapse -->
+                    </nav>
+                </div>
+                <!-- / menu-skew-div -->
+            </div>
+            <!-- /.container main-menu -->
+        </div>
+        <!-- /.main-menu -->
+    </section>
