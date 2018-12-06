382d8b542ef5d6a4b3ea9e1e50cd2da624c58e31
Amyotte <lorenk45@uwosh.edu>
2018-12-06 16:07:09 -0600

    Austins cat/dog listing pages and add listing page

---
+++ admin/add-listing.php
@@ -0,0 +1,113 @@
+<?php
+set_include_path("/var/www/students/team8/");
+include("shared/header.php");
+include("shared/navbar.php");
+if (!isset($_SESSION["role"]) || $_SESSION["role"] != "admin")
+{
+    header("location: http://webdev.cs.uwosh.edu/students/team8/index.php");
+	exit;
+}
+include("database/database.php");
+
+$success = true;
+function getInput($name) {
+	$input = htmlspecialchars($_POST[$name]);
+	if ($input != "") {
+		return $input;
+	} else {
+		$success = false;
+	}
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$name = getInput("name");
+	$gender = getInput("gender");
+	$isDog = getInput("species") == "dog" ? 1 : 0;
+	$breed = getInput("breed");
+	//$birthDate = date("Y-m-d", strtotime(getInput("birthDate")));
+	$birthDate = getInput("birthDate");
+	$size = getInput("size");
+	$color = getInput("color");
+	$fileName = "";
+	if(is_uploaded_file($_FILES["image"]["tmp_name"]) && getimagesize($_FILES["image"]["tmp_name"]) != false) {
+		$uploads = "/var/www/students/team8/images/uploads";
+		$fileName = basename($_FILES["image"]["name"]);
+		move_uploaded_file($_FILES["image"]["tmp_name"], "$uploads/$name");
+	} else {
+		$success = false;
+	}
+	$fixed = 0;
+	if (key_exists("checkbox1", $_POST) && $_POST["checkbox1"] == "fixed") {
+			$fixed = 1;
+	}
+	$declawed = 0;
+	if (key_exists("checkbox2", $_POST) && $_POST["checkbox2"] == "declawed") {
+			$declawed = 1;
+	}
+	$houseTrained = 0;
+	if (key_exists("checkbox3", $_POST) && $_POST["checkbox3"] == "housetrained") {
+			$houseTrained = 1;
+	}
+	$site = getInput("site");
+	$location = getInput("location");
+	//$intakeDate = date("Y-m-d", strtotime(getInput("intakeDate")));
+	$intakeDate = getInput("intakeDate");
+	$description = getInput("description");
+	if ($success) {
+		$db = db_connect();
+		$db->exec("INSERT INTO Animals (Name, Gender, IsDog, Breed, BirthDate, Color, Size, Picture, 
+		Fixed, Declawed, Housetrained, Site, Location, IntakeDate, Description) VALUES (".$db->quote($name).", ".
+		$db->quote($gender).", ".$db->quote($isDog).", ".$db->quote($breed).", ".$db->quote($birthDate).", ".
+		$db->quote($color).", ".$db->quote($size).", ".$db->quote("http://webdev.cs.uwosh.edu/students/team8/images/uploads/$name")
+		.", ".$db->quote($fixed).", ".$db->quote($declawed).", ".$db->quote($houseTrained).", ".$db->quote($site).", ".
+		$db->quote($location).", ".$db->quote($intakeDate).", ".$db->quote($description).");");
+		db_disconnect($db);
+	}
+}
+?>
+<h2>Add an Animal Listing</h2>
+<form action="add-listing.php" method="POST" enctype="multipart/form-data">
+	<label for="name">Name:</label>
+	<input type="text" name="name"><br>
+	<label for="gender">Gender:</label>
+	<input type="text" name="gender"><br>
+	<label for="species">Species:</label>
+	<select name="species">
+		<option value="dog">Dog:</option>
+		<option value="cat">Cat:</option>
+	</select><br>
+	<label for="breed">Breed:</label>
+	<input type="text" name="breed"><br>
+	<label for="birthDate">Birth Date:</label>
+	<input type="date" name="birthDate"><br>
+	<label for="size">Size:</label>
+	<input type="text" name="size"><br>
+	<label for="color">Color:</label>
+	<input type="text" name="color"><br>
+	<div>
+	<label for="image">Picture:</label>
+	<input type="file" name="image" accept="image/*">
+	</div>
+	<label for="checkbox1"></label>
+	<input type="checkbox" name="checkbox1" value="fixed"> Fixed<br>
+	<label for="checkbox2"></label>
+	<input type="checkbox" name="checkbox2" value="declawed"> Declawed<br>
+	<label for="checkbox3"></label>
+	<input type="checkbox" name="checkbox3" value="housetrained"> Housetrained<br>
+	<label for="site">Site:</label>
+	<input type="text" name="site"><br>
+	<label for="Location">Location:</label>
+	<input type="text" name="location"><br>
+	<label for="intakeDate">Intake Date:</label>
+	<input type="date" name="intakeDate"><br>
+	<label for="description">Description:</label>
+	<input type="text" name="description"><br>
+	<label for="submit"></label>
+	<input type="submit" value="Submit">
+</form>
+<?php if($_SERVER["REQUEST_METHOD"] == "POST" && $success){
+	?><p>Listing successfully added.</p><?php
+} else if($_SERVER["REQUEST_METHOD"] == "POST"){
+	?><p>Listing could not be added. Make sure all form fields are filled out.</p><?php
+}
+include("shared/footer.php");?>
--- dogs.php
+++ dogs.php
@@ -1,7 +1,7 @@
-<?php include("shared/header.php"); ?>
+<?php include("database/database.php");
+include("shared/header.php"); ?>
 <!-- put custom style sheets here -->
 <?php include("shared/navbar.php"); ?>
-  
   <!-- Theme Inner Banner ____________________________ -->
   <section class="">
     <div class="them-inner-banner">
@@ -11,7 +11,7 @@
             <div class="row">
               <div class="col-sm-12">
                 <div class="">
-                  <h1>Cats</h1>
+                  <h1>Adoptable Dogs</h1>
                   <p>Pet lovers rely on SavingPaws Pet Care for professional dog and cat walking and pet sitting.</p>
                 </div>
               </div>
@@ -23,11 +23,11 @@
     <div class="inner-banner-bottom">
       <div class="container">
         <ul>
-          <li><a href="index.html">Home</a></li>
+          <li><a href="index.php">Home</a></li>
           <li><span>-</span></li>
-          <li><a href="adoptions.html">Adoptions</a></li>
+          <li><a href="adoptions.php">Adoptions</a></li>
           <li><span>-</span></li>
-          <li><a href="#">Cats</a></li>
+          <li><a href="#">Dogs</a></li>
         </ul>
       </div>
     </div>
@@ -37,108 +37,118 @@
       <div class="row">
         <div class="col-md-12">
           <div class="we-are-my-pet-text"> <span></span>
-            <h2>Adoptable Cats</h2>
-            <p>Cats: standard treatments include spay/neuter, deworming, a combo test for feline leukemia and FIV, distemper vaccine, rabies vaccine, seasonal topical treatments for fleas, ticks, and mites, additional treatments as needed, and 30 days free ShelterCare pet insurance.  Adopters may not four-paw declaw cats and kittens adopted from Saving Paws.</p>
-            <p>Some treatments are not age appropriate and kittens cannot have all the above standard treatments/tests. Spay/Neuter will be set up by Saving Paws with one of our vet partners when the kitten is 6 months of</p>
+            <h2>Adoptable Dogs</h2>
+			<?php $db = db_connect();
+			$dogs = $db->query("SELECT * FROM Animals WHERE IsDog = 1;");
+			foreach($dogs as $dog){
+				?><img src="<?php echo($dog["Picture"]); ?>" alt="A dog" class="float-left" height="225">
+				<p>Name: <?php echo($dog["Name"]); ?><br>
+				Gender: <?php echo($dog["Gender"]); ?><br>
+				Breed: <?php echo($dog["Breed"]); ?><br>
+				Size: <?php echo($dog["Size"]); ?><br>
+				Color: <?php echo($dog["Color"]); ?><br>
+				Birth Date: <?php echo($dog["BirthDate"]); ?></p>
+				<p>Description: <?php echo($dog["Description"]); ?></p>
+				<p>Fixed: <?php if(ord($dog["Fixed"])==1) {echo("Yes");} else {echo("No");} ?>
+				&emsp;Declawed: <?php if(ord($dog["Declawed"])==1) {echo("Yes");} else {echo("No");} ?>
+				&emsp;Housetrained: <?php if(ord($dog["Housetrained"])==1) {echo("Yes");} else {echo("No");} ?></p>
+				<p>Site: <?php echo($dog["Site"]); ?></p>
+				<p>Location: <?php echo($dog["Location"]); ?></p>
+				<p>Intake Date: <?php echo($dog["IntakeDate"]); ?></p>
+				<br><?php
+			}
+			db_disconnect($db);
+			?>
             <section class="pet-services-v-one">
 				<div class="container">
 					<div class="row our-pet-services">
-						<div class="col-md-4 col-sm-6 col-xs-12">
-							<div class="our-pet-services-item">
-								<div class="our-pet-services-img">
-									<img src="images/home/img-2.jpg" alt="image">
-								</div>
-								<div class="our-pet-services-text">
-									<h5><a href="service-details.html">PET ADOPTION</a></h5>
-									<p>Whether your pet needs a scratch around the ear, a face to lick or a place to play,</p>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4 col-sm-6 col-xs-12">
+						<div class="col-md-3 col-sm-6 col-xs-12">
 							<div class="our-pet-services-item">
 								<div class="our-pet-services-img">
 									<img src="images/home/img-3.jpg" alt="image">
 								</div>
 								<div class="our-pet-services-text">
 									<h5><a href="service-details.html">PET GROOMING</a></h5>
-									<p>Best Friends Pet Care is the leader of the pack when it comes to the absolute best care for your cat or dog.</p>
+								</div>
+							</div>
+							<div class="pet-overlay">
+								<div class="overlay-pet-services-img">
+									<img src="images/home/img-3.jpg" alt="image">
+								</div>
+								<div class="overlay-services-text">
+									<h5><a href="pet1-details.html">PET GROOMING</a></h5>
+									<p>Puppy : <span>Male</span></p>
+									<p>Yigo</p>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-4 col-sm-6 col-xs-12">
+						<div class="col-md-3 col-sm-6 col-xs-12">
 							<div class="our-pet-services-item">
 								<div class="our-pet-services-img">
 									<img src="images/home/img-4.jpg" alt="image">
 								</div>
 								<div class="our-pet-services-text">
 									<h5><a href="service-details.html">Pet Health care</a></h5>
-									<p>In a professional context it often happens that private or corporate clients order a publication</p>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-4 col-sm-6 col-xs-12">
+						<div class="col-md-3 col-sm-6 col-xs-12">
 							<div class="our-pet-services-item">
 								<div class="our-pet-services-img">
 									<img src="images/home/img-5.jpg" alt="image">
 								</div>
 								<div class="our-pet-services-text">
 									<h5><a href="service-details.html">DOG WALKING</a></h5>
-									<p>Whether your pet needs a scratch around the ear, a face to lick or a place to play,</p>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-4 col-sm-6 col-xs-12">
+						<div class="col-md-3 col-sm-6 col-xs-12">
 							<div class="our-pet-services-item">
 								<div class="our-pet-services-img">
 									<img src="images/home/img-6.jpg" alt="image">
 								</div>
 								<div class="our-pet-services-text">
 									<h5><a href="service-details.html">PET DAYCARE</a></h5>
-									<p>Best Friends Pet Care is the leader of the pack when it comes to the absolute best care for your cat or dog.</p>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-4 col-sm-6 col-xs-12">
+						<div class="col-md-3 col-sm-6 col-xs-12">
 							<div class="our-pet-services-item">
 								<div class="our-pet-services-img">
 									<img src="images/home/img-7.jpg" alt="image">
 								</div>
 								<div class="our-pet-services-text">
 									<h5><a href="service-details.html">Nice Product</a></h5>
-									<p>In a professional context it often happens that private or corporate clients order a publication</p>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-4 col-sm-6 col-xs-12">
+						<div class="col-md-3 col-sm-6 col-xs-12">
 							<div class="our-pet-services-item">
 								<div class="our-pet-services-img">
 									<img src="images/services/img-7.jpg" alt="image">
 								</div>
 								<div class="our-pet-services-text">
 									<h5><a href="service-details.html">Puppy Program</a></h5>
-									<p>Whether your pet needs a scratch around the ear, a face to lick or a place to play,</p>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-4 col-sm-6 col-xs-12">
+						<div class="col-md-3 col-sm-6 col-xs-12">
 							<div class="our-pet-services-item">
 								<div class="our-pet-services-img">
 									<img src="images/services/img-8.jpg" alt="image">
 								</div>
 								<div class="our-pet-services-text">
 									<h5><a href="service-details.html">Monthly Care Contracts</a></h5>
-									<p>Best Friends Pet Care is the leader of the pack when it comes to the absolute best care for your cat or dog.</p>
 								</div>
 							</div>
 						</div>
-						<div class="col-md-4 col-xs-12">
+						<div class="col-md-3 col-xs-12">
 							<div class="our-pet-services-item">
 								<div class="our-pet-services-img">
 									<img src="images/services/img-9.jpg" alt="image">
 								</div>
 								<div class="our-pet-services-text">
 									<h5><a href="service-details.html">Pet Care</a></h5>
-									<p>In a professional context it often happens that private or corporate clients order a publication</p>
 								</div>
 							</div>
 						</div>
@@ -151,4 +161,4 @@
     </div>
   </section>
     <!-- put custom js here -->
-    <?php include("shared/footer.php");?>
+    <?php include("shared/footer.php");?></html>
